6fe397e55fedee737543a51e55b8355d738a5255
keenanmcentee <c00201892@itcarlow.ie>
2017-12-05 10:10:04 +0000

    Base Game loop

---
+++ Main.js
@@ -0,0 +1,20 @@
+/** 
+ *  @fileOverview Create an instance of game.
+ *
+ *  @author       Keenan McEntee
+ */
+
+/**
+ * Creates an instance of Game and calls initialise on it.
+ */
+var gameNs = {};
+
+function main()
+{
+	
+	const game = new Game();
+	gameNs.game = game;
+	game.initWorld();
+	game.update();
+
+}

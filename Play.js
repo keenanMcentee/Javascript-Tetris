78d5a19a0f4a31dc06089f63e5865c3b94b18231
keenanmcentee <c00201892@itcarlow.ie>
2017-12-07 16:49:39 +0000

    update to the gameplay

--- Play.js
+++ Play.js
@@ -5,31 +5,52 @@ class Play extends Scene
 		super(title);
 		this.play = this;
 		this.grid = new Grid();
-		//Blocks ID
-		this.blocks = {I: 0, J: 1, L: 2, O: 3, T: 4, S: 5, Z: 6};
-		this.jBlock = new Blocks();
+		this.gridMatrix = this.grid.createMatrix(this.grid.rows,this.grid.columns);
+		this.player = new Blocks();
 		this.timeSinceLastFrame = 0;
 		this.move = {x: null, y: null};
-		document.addEventListener("touchstart", this.onTouchStart.bind(null, this));
-		document.addEventListener('touchmove', this.onTouchMove.bind(null, this));
+		if (is_touch_device)
+		{
+			console.log("Touch");
+			document.addEventListener("touchstart", this.onTouchStart.bind(null, this));
+			document.addEventListener('touchmove', this.onTouchMove.bind(null, this));
+		}
+		else
+		{
+			console.log("NoTouch");
+			document.addEventListener("keydown", keyDownHandler.bind(null, this));
+		}
+		
 	}
 	update(dt)
 	{
+		
 		this.timeSinceLastFrame += dt/1000;
 		if (this.timeSinceLastFrame > 1.5)
 		{
 			this.timeSinceLastFrame = 0;
-			this.jBlock.offset.y += 1;
+			this.blockDrop();
 		}
 	}
 	render(ctx)
 	{
 		
 		this.blockSize = {width: ((ctx.canvas.width - 200) / this.grid.rows) , height: ((ctx.canvas.height - 200) / this.grid.columns)}
-		this.jBlock.draw(ctx, this.blockSize);
+		this.player.draw(ctx, this.blockSize);
 		this.grid.draw(ctx, this.blockSize);
 	}
-
+	blockDrop()
+	{
+		this.player.offset.y += 1;
+		if (this.grid.collide(this.player))
+		{
+			//console.log("collided");
+			//this.player.offset.y--;
+			//this.grid.merge(this.player);
+			//this.player.offset.y = 0;
+			//console.table(this.grid.matrix);
+		}
+	}
 	
 	onTouchStart(program, e)
 	{
@@ -52,14 +73,14 @@ class Play extends Scene
 		if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
 			if ( xDiff > 3 ) {
 				console.log("Left swipe");
-				if (program.jBlock.offset.x > 0)
+				if (program.player.offset.x > 0)
 				{
-					program.jBlock.offset.x--;
+					program.player.offset.x--;
 				}
 			} else if (xDiff < 3){
-				if (program.jBlock.offset.x < program.grid.rows)
+				if (program.player.offset.x < program.grid.rows)
 				{
-					program.jBlock.offset.x++;
+					program.player.offset.x++;
 				}
 				console.log("Right swipe");
 			}
@@ -80,4 +101,30 @@ class Play extends Scene
 		program.move.x = null;
 		program.move.y = null;               
 	}
+		
+		/**
+	 * Event function that is triggered when a key is pressed.
+	 */
+	keyDownHandler (program, e)
+	{
+		//code triggered when UP arrow is pressed
+		if(e.keyCode === 37)
+		{	//LEFT
+			console.log("Left");
+			program.player.offset.x--;
+		}
+		if(e.keyCode === 38)
+		{	//UP
+			
+		}
+		if(e.keyCode === 39)
+		{	//RIGHT
+			program.player.offset.x++;
+		}
+		if(e.keyCode === 40)
+		{	//DOWN
+			
+		}
+	}
+
 }
